e1c09889d2f01373132dadd84427d07c0a078d83
AnthonyChacin <anthony.chacin@correo.unimet.edu.ve>
2018-10-12 14:27:22 -0400

    Agregando lista de pokémons

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -4,8 +4,7 @@ import { DataBaseService } from 'src/app/service/data-base.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'],
-  providers: [DataBaseService]
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
